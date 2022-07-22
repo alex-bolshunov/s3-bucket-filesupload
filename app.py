687f0cd55bd7b0dd17a30d5ddc5f17c4a07ab6ac
Oleksii Bolshunov <bolshunov_oleksii@ukr.net>
2022-07-21 23:40:51 -0400

    add command line argument

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pydoc import cli
 import boto3
+import sys
 from dotenv import load_dotenv
 from os import getenv
 from upload_download import push
@@ -18,8 +19,13 @@ bucket_name = getenv('BUCKET_NAME')
 # s3 client
 client = boto3.client('s3', endpoint_url = endpoint_url)
 
-# #upoad files
-status = push(path, bucket_name, client)
+#get overwrite
+overwrite =  sys.argv[1].lower() == 'true' if len(sys.argv) > 1 else False
 
-if status: print('Files have been uploaded')
-else: print('Smth went wrong')
+#upoad files
+try:
+    push(path, bucket_name, client, overwrite)
+    print('Files have been uploaded')
+except: 
+    print('Smth went wrong')
+
